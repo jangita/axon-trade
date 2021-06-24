739e3d4001f1627a0c76034702b3c43405d0d7d2
Chris Ochieng <jangita.nyagudi@gmail.com>
2021-06-24 16:01:35 +0000

    Direct HTTP API

--- app/views.py
+++ app/views.py
@@ -1,11 +1,34 @@
+from flask import request
 from app import app
+from os import environ
+import hmac
+import hashlib
+import requests
+import datetime
+import binascii
 
 
-@app.route("/")
-def index():
-    return "Hello world"
+def hmac_sha256(key, message):
+    byte_key = key.encode()
+    message = message.encode()
+    return hmac.new(byte_key, message, hashlib.sha256).hexdigest()
 
 
-@app.route("/about")
-def about():
-    return "All about Flask"
+@app.route("/test", methods=['GET'])
+def test():
+    api_key = environ['binance_api_key']
+    api_secret = environ['binance_api_secret']
+
+    headers = {'X-MBX-APIKEY': api_key}
+
+    params = 'timestamp=' + \
+        str(int(datetime.datetime.now().timestamp() * 1000))
+
+    signature = hmac_sha256(api_secret, params)
+
+    url = 'https://api.binance.com/api/v3/account?' + \
+        params + '&signature=' + signature
+    print(url)
+
+    response = requests.get(url, headers=headers)
+    return response.text, 200
