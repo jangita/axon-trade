f4533d76c2eb39630f006b07d87ea762e650374e
Chris Ochieng <jangita.nyagudi@gmail.com>
2021-06-25 12:22:07 +0000

    Change to POST / content body

--- app/views.py
+++ app/views.py
@@ -28,7 +28,30 @@ def test():
 
     url = 'https://api.binance.com/api/v3/account?' + \
         params + '&signature=' + signature
-    print(url)
 
     response = requests.get(url, headers=headers)
+    return response.text, 200
+
+
+@app.route("/a", methods=['GET'])
+def a():
+    api_key = environ['binance_api_key']
+    api_secret = environ['binance_api_secret']
+
+    headers = {'X-MBX-APIKEY': api_key}
+    json = request.get_json()
+
+    symbol = json['symbol']
+    side = json['side']
+    quantity = json['quantity']
+
+    params = 'symbol=' + symbol + '&side=' + side + \
+        '&quantity=' + quantity + '&type=MARKET&timestamp=' + \
+        str(int((datetime.datetime.now() - datetime.timedelta(hours=-4)).timestamp() * 1000))
+
+    signature = hmac_sha256(api_secret, params)
+
+    params = params + '&signature=' + signature
+    url = 'https://api.binance.com/api/v3/order/test'
+    response = requests.post(url, headers=headers, data=params)
     return response.text, 200
